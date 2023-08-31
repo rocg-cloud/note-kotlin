2079edcb0498ba2c2efd5dc1ad61e6a19972b9d3
rocwang <842875195@qq.com>
2023-08-31 16:18:30 +0800

    github:deploy.yml

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import {DefaultTheme, defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: `/kotlin-in-action-doc/`,
+  base: "/note-kotlin/",
   title: "kotlin-in-action",
   description: "kotlin-in-action",
 
